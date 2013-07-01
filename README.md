e61289cd8903f2d7132d4451df1fedfd6001b6be
Muslim Idris <gelamu@gmail.com>
2013-07-01 05:16:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-node-curly-colors
-=================
+# node-curly-colors
 
-Simplifies the working with console colors.
+Simplifies the working with console colors in Node.js
+
+## Installation
+
+    $ npm install curly-colors
+
+## How to include
+
+```js
+    var cc = require('curly-colors');
+```
+
+## Available Color
+
+- `black`
+- `red`
+- `green`
+- `yellow`
+- `blue`
+- `magenta`
+- `cyan`
+- `white`
+- `grey`
+
+## Available Styles
+
+- `bold` for bold size text
+- `italic` for italic text
+- `underline` for underlined text
+- `blink` for blinking text
+- `inverse` to inverse the colors
+
+## Examples
+
+### Simple colors
+
+```js
+    console.log(cc('<{red>red text<}>'));
+```
+
+### Multiple colors
+
+```js
+    console.log(cc('<{red>red text<}> <{green>green text<}>'));
+```
+
+### Nested colors
+
+```js
+    console.log(cc('<{red>red text <{green>green text<}> red text<}>'));
+    console.log(cc('<{red>red <{green>green <{bold>bold green<}> green<}> red<}>'));
+```
+
+### And the final console output looks like this
+![Console Screenshot](http://i.imgur.com/5p2AhjL.png)
---
+++ test/test.js
@@ -0,0 +1,38 @@
+var cc = require('..');
+var assert = require('assert');
+
+describe('curly-colors', function() {
+
+  describe('Simple colors (not nested)', function() {
+
+    it('should return only red', function() {
+      assert.equal('\u001b[31mfoo\u001b[39m', cc('<{red>foo<}>'));
+      assert.equal('\u001b[31mfoo\u001b[39m', cc('<{red>%s<}>', 'foo'));
+    });
+
+    it('should return a string containing red', function() {
+      assert.equal('default \u001b[31mfoo\u001b[39m default',
+                   cc('default <{red>foo<}> default'));
+      assert.equal('default \u001b[31mfoo\u001b[39m default',
+                   cc('default <{red>%s<}> default', 'foo'));
+    });
+
+    it('should return a string containing red and green', function() {
+      assert.equal('default \u001b[31mfoo\u001b[39m default \u001b[32mbar\u001b[39m default',
+                   cc('default <{red>foo<}> default <{green>bar<}> default'));
+      assert.equal('default \u001b[31mfoo\u001b[39m default \u001b[32mbar\u001b[39m default',
+                   cc('default <{red>%s<}> default <{green>%s<}> default', 'foo', 'bar'));
+    });
+
+  });
+
+  describe('Nested colors', function() {
+
+    it('should return a string with yellow and magenta on green', function() {
+      assert.equal('\u001b[32mgreen \u001b[33myellow\u001b[39m\u001b[32m green \u001b[35mmagenta\u001b[39m\u001b[32m green\u001b[39m',
+                   cc('<{green>green <{yellow>yellow<}> green <{magenta>magenta<}> green<}>'));
+    });
+
+  });
+
+});
